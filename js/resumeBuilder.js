4f1d730909e0a37e9b6e99685ca0708d4bb579b1
therealgilgordon <gil.gordon@gmail.com>
2014-12-11 10:35:25 -0800

    Added Json data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,88 @@
-$("#main").append(["Gil Gordon"]);
+//My Data
+var bio = {
+	"name" 			: "Gil Gordon",
+	"role" 			: "Product Manager",
+	"contacts" 		: {
+		"mobile number"    	: "408-836-8147",
+		"email"			: "gil.gordon@gmail.com",
+		"github"		: "therealgilgordon",
+		"twitter"		: "@gilgordon",
+		"location"		: "San Mateo, CA"
+	},
+	"bioPic"	: "images/me.jpg",
+	"welcomeMessage": "Welcome To my page. Why are you here?",
+	"skills"		: [
+		"All the things", 
+		"I am aware of all Internet traditions",
+		"HTML5",
+		"Product Management",
+		"Program Management"
+	]
+};
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "Sencha Inc.",
+		"title": "Senior Product Manager - Tools",
+		"location": "Redwood City, CA",
+		"dates worked": "2012-2014",
+		"description": "Managed product development of suite of HTML5 developer tools"
+	},
+		{
+		"employer" : "Microsoft",
+		"title": "Program Manager",
+		"location": "Mountain View, CA",
+		"dates worked": "1997-2007",
+		"description": "Entered as QA contractor for Mac IE/OE. Then PM Lead for Mac Entourage. Then PM Lead on Hotmail Front End."
+	}
+	]
+};
+
+var projects = {
+	"projects" : [
+		{
+
+			"title" : "Intro to Html and CSS",
+			"dates worked" : "November 2014",
+			"description" : "A little bit of Bootstrap",
+			"images" :
+			[
+				"images/me.jpg",
+				"images/me.jpg"
+			]
+		},
+		{
+			"title" : "JavaScript Basics",
+			"dates worked" : "December 2014",
+			"description" : "A little review",
+			"images" :
+			[
+				"images/me.jpg",
+				"images/me.jpg"
+			]
+		}
+	]
+};
+
+
+var education = {
+	"schools" : [
+	{
+		"name" : "UCLA",
+		"city" : "Los Angeles, CA, US",
+		"degree" : "BS",
+		"majors"  : ["Cognitive Science"],
+		"dates attended" : "1991-1995",
+		"url" : "http://www.ucla.edu"
+	}
+	],
+	"onlineCourses" : [
+	{
+		"school" : "uDacity",
+		"city" : "online",
+		"dates attended" : "2014-present",
+		"title"  : "Front End Web Devloper NanoDegree"
+	}
+	]	
+};
